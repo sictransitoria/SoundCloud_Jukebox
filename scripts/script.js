b92f2ae6be977d4a4df8aebc2860c2810f3a6504
Michael Oria <Michael_Oria@me.com>
2018-03-02 09:54:04 -0500

    Updates made to SoundCloud_Jukebox including Professor Adu's code.

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+// var Jukebox = {
+// 	songs: [],
+// 	activeSong: null,
+// 	activeAudio: null,
+// 	isPlaying: false,
+// 	dom: {},
+
+// }
+
+
+// Custom Scripts
+
+var x = document.getElementById("audioPlayer"); 
+function playAudio() { 
+    x.play(); 
+
+} 
+
+function pauseAudio() { 
+    x.pause(); 
+
+} 
+
+function nextAudio() { 
+    x.next(); 
+
+} 
+
+function previousAudio() { 
+    x.prev(); 
+
+} 
